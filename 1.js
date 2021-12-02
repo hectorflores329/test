a40b7047cdfc282f77b197262d4acdc7f5f43cb2
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-12-02 00:54:02 -0600

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,291 @@
+puntos = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.73533421300002,
+                    -33.89377388000002
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13404",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Paine",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "TENENCIA CARRETERAS - TCIA. CARRET. PAINE",
+                "POINT_X": -70.735334213,
+                "POINT_Y": -33.89377388
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.88485377999997,
+                    -33.87728522299999
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13404",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Paine",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "RETEN - RETEN PINTUE",
+                "POINT_X": -70.88485378,
+                "POINT_Y": -33.877285223
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.76358703799998,
+                    -33.85498578800002
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13404",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Paine",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "TENENCIA - TCIA. CHAMPA",
+                "POINT_X": -70.763587038,
+                "POINT_Y": -33.854985788
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.64183944799998,
+                    -33.83244324000002
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13404",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Paine",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "RETEN - RETEN HUELQUEN",
+                "POINT_X": -70.641839448,
+                "POINT_Y": -33.83244324
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.741648,
+                    -33.807868999999954
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13404",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Paine",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "COMISARIA - 64A. COM. PAINE",
+                "POINT_X": -70.741648,
+                "POINT_Y": -33.807869
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.79154057199996,
+                    -33.78242384400004
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13402",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Buin",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "TENENCIA - TCIA. VILUCO",
+                "POINT_X": -70.791540572,
+                "POINT_Y": -33.782423844
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.73563199999997,
+                    -33.759820000000026
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13402",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Buin",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "TENENCIA - TCIA. LINDEROS",
+                "POINT_X": -70.735632,
+                "POINT_Y": -33.75982
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.68404361700001,
+                    -33.73200157200004
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13402",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Buin",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "TENENCIA - TCIA. ALTO JAHUEL",
+                "POINT_X": -70.684043617,
+                "POINT_Y": -33.732001572
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.77475199999998,
+                    -33.729496000000026
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13402",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Buin",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "SUBCOMISARIA - SCOM. MAIPO",
+                "POINT_X": -70.774752,
+                "POINT_Y": -33.729496
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.74481999999999,
+                    -33.728170999999996
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13402",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Buin",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "COMISARIA - 15A. COM. BUIN",
+                "POINT_X": -70.74482,
+                "POINT_Y": -33.728171
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.75826999999995,
+                    -33.630996000000025
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13403",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Calera de Tango",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "SUBCOMISARIA - SCOM. CALERA DE TANGO",
+                "POINT_X": -70.75827,
+                "POINT_Y": -33.630996
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.68677900000003,
+                    -33.594302000000006
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13401",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "San Bernardo",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "COMISARIA - 62A. COM. SAN BERNARDO",
+                "POINT_X": -70.686779,
+                "POINT_Y": -33.594302
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -70.70129022199998,
+                    -33.5921081856
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13401",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "San Bernardo",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "COMISARIA - 14A. COM. SAN BERNARDO",
+                "POINT_X": -70.701290222,
+                "POINT_Y": -33.5921081856
+            }
+        }
+    ]
+}
