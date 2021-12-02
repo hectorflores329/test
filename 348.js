38a3bb150405b4d6facafda5999828f81266248b
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-12-02 02:51:14 -0600

    Commit

--- 348.js
+++ 348.js
@@ -10652,7 +10652,7 @@ comuna = {
             },
             "properties": {
                 "NOM_COMUNA": "SANTIAGO",
-                "ESTABLECIM": "6"
+                "ESTABLECIM": "13"
             }
         }
     ]
