c54f1b04120ec27f9e4c966bc891805a7de811ef
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-12-02 01:51:31 -0600

    Update puntos3.5.js

--- puntos3.5.js
+++ puntos3.5.js
@@ -1,1 +1,3 @@
-puntos = {"type": "FeatureCollection","features": [{"type": "Feature","geometry": {"type": "Point","coordinates": [-70.672470209547,-33.4533343033513],"properties": {"COMUNA": "SANTIAGO", "ESTABLECIMIENTOS": "2"}}, {"type": "Feature","geometry": {"type": "Point","coordinates": [-70.6606255744389,-33.4547665225854],"properties": {"COMUNA": "SANTIAGO", "ESTABLECIMIENTOS": "3"}}}
+puntos = {"type": "FeatureCollection","features": [
+{"type": "Feature","geometry": {"type": "Point","coordinates": [-70.672470209547,-33.4533343033513]},"properties": {"COMUNA": "SANTIAGO", "ESTABLECIMIENTOS": "2"}}, {"type": "Feature","geometry": {"type": "Point","coordinates": [-70.6606255744389,-33.4547665225854]},"properties": {"COMUNA": "SANTIAGO", "ESTABLECIMIENTOS": "3"}}
+]}
