49390a5e8eaae11ddfd0738879e826c85d193ff8
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-12-02 01:29:31 -0600

    Update and rename puntos1.js to puntos2.js

---
+++ puntos2.js
@@ -0,0 +1,1 @@
+puntos = {"type": "FeatureCollection","features": [{"type": "Feature","geometry": {"type": "Point","coordinates": [-72.66731651115752,-38.03749999657841]},"properties": {"COMUNA": "SANTIAGO", "ESTABLECIMIENTOS": "2"}}]}
