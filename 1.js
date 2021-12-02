c8c50fa968c4080bf0feecac504255f83d9a3c52
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-12-02 01:06:02 -0600

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,13 +6,66 @@ puntos = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    -70.73533421300002,
-                    -33.89377388000002
+                    -72.66731651115752,
+                    -38.03749999657841
                 ]
             },
             "properties": {
-                "ESABLECIMIENTOS": "13"
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13404",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Paine",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "TENENCIA CARRETERAS - TCIA. CARRET. PAINE",
+                "POINT_X": -70.735334213,
+                "POINT_Y": -33.89377388
             }
         },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -72.67282119232824,
+                    -38.48333332987164
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13401",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "San Bernardo",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "COMISARIA - 14A. COM. SAN BERNARDO",
+                "POINT_X": -70.701290222,
+                "POINT_Y": -33.5921081856
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -72.65282119232824,
+                    -38.48333332987164
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13401",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "San Bernardo",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "COMISARIA - 14A. COM. SAN BERNARDO",
+                "POINT_X": -70.701290222,
+                "POINT_Y": -33.5921081856
+            }
+        }
     ]
 }
